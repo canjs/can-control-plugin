8a0a4a119790abe9d2ccc45548e7a0981e23a966
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:46:51 +0100

    Landscaper: QUnit2 upgrade

--- src/can-control-plugin_test.js
+++ src/can-control-plugin_test.js
@@ -7,21 +7,21 @@ require('steal-qunit');
 
 QUnit.module('can/control/plugin');
 
-test('pluginName', function() {
-	expect(8);
+QUnit.test('pluginName', function(assert) {
+	assert.expect(8);
 	can.Control('My.TestPlugin', {
 		pluginName: 'my_plugin'
 	}, {
 		init: function(el, ops) {
-			ok(true, 'Init called');
-			equal(ops.testop, 'testing', 'Test argument set');
+			assert.ok(true, 'Init called');
+			assert.equal(ops.testop, 'testing', 'Test argument set');
 		},
 		method: function(arg) {
-			ok(true, 'Method called');
-			equal(arg, 'testarg', 'Test argument passed');
+			assert.ok(true, 'Method called');
+			assert.equal(arg, 'testarg', 'Test argument passed');
 		},
 		update: function() {
-			ok(true, 'Update called');
+			assert.ok(true, 'Update called');
 		}
 	});
 	var ta = can.$('<div/>')
@@ -31,7 +31,7 @@ test('pluginName', function() {
 		testop: 'testing'
 	});
 	// Init
-	ok(ta.hasClass('my_plugin'), 'Should have class my_plugin');
+	assert.ok(ta.hasClass('my_plugin'), 'Should have class my_plugin');
 	ta.my_plugin();
 	// Update
 	ta.my_plugin('method', 'testarg');
@@ -39,21 +39,21 @@ test('pluginName', function() {
 	ta.control()
 		.destroy();
 	// destroy
-	ok(!ta.hasClass('my_plugin'), 'Shouldn\'t have class my_plugin after being destroyed');
-	ok(ta.hasClass('existing_class'), 'Existing class should still be there');
+	assert.ok(!ta.hasClass('my_plugin'), 'Shouldn\'t have class my_plugin after being destroyed');
+	assert.ok(ta.hasClass('existing_class'), 'Existing class should still be there');
 });
-test('.control(), .controls() and _fullname', function() {
-	expect(3);
+QUnit.test('.control(), .controls() and _fullname', function(assert) {
+	assert.expect(3);
 	can.Control('My.TestPlugin', {});
 	var ta = can.$('<div/>')
 		.appendTo($('#qunit-fixture'));
-	ok(ta.my_test_plugin, 'Converting Control name to plugin name worked');
+	assert.ok(ta.my_test_plugin, 'Converting Control name to plugin name worked');
 	ta.my_test_plugin();
-	equal(ta.controls()
+	assert.equal(ta.controls()
 		.length, 1, '.controls() returns one instance');
-	ok(ta.control() instanceof My.TestPlugin, 'Control is instance of test plugin');
+	assert.ok(ta.control() instanceof My.TestPlugin, 'Control is instance of test plugin');
 });
-test('update', function() {
+QUnit.test('update', function(assert) {
 	can.Control({
 		pluginName: 'updateTest'
 	}, {});
@@ -65,10 +65,10 @@ test('update', function() {
 	ta.updateTest({
 		testop: 'testing'
 	});
-	equal(ta.control()
+	assert.equal(ta.control()
 		.options.testop, 'testing', 'Test option has been extended properly');
 });
-test('calling methods', function() {
+QUnit.test('calling methods', function(assert) {
 	can.Control({
 		pluginName: 'callTest'
 	}, {
@@ -82,10 +82,10 @@ test('calling methods', function() {
 	var ta = can.$('<div/>')
 		.appendTo($('#qunit-fixture'));
 	ta.callTest();
-	ok(ta.callTest('setName', 'Tester') instanceof $, 'Got jQuery element as return value');
-	equal(ta.callTest('returnTest'), 'Hi Tester', 'Got correct return value');
+	assert.ok(ta.callTest('setName', 'Tester') instanceof $, 'Got jQuery element as return value');
+	assert.equal(ta.callTest('returnTest'), 'Hi Tester', 'Got correct return value');
 });
-test('always use pluginName first in .control(name) (#448)', 4, function() {
+QUnit.test('always use pluginName first in .control(name) (#448)', 4, function(assert) {
 	can.Control('SomeName', {
 		pluginName: 'someTest'
 	}, {});
@@ -97,9 +97,9 @@ test('always use pluginName first in .control(name) (#448)', 4, function() {
 	ta.someTest();
 	ta.otherTest();
 	var control = ta.control('someTest');
-	ok(control, 'Got a control from pluginName');
-	equal(control.constructor.pluginName, 'someTest', 'Got correct control');
+	assert.ok(control, 'Got a control from pluginName');
+	assert.equal(control.constructor.pluginName, 'someTest', 'Got correct control');
 	control = ta.control('otherTest');
-	ok(control, 'Got a control from pluginName');
-	equal(control.constructor.pluginName, 'otherTest', 'Got correct control');
+	assert.ok(control, 'Got a control from pluginName');
+	assert.equal(control.constructor.pluginName, 'otherTest', 'Got correct control');
 });
